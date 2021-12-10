39efd0b26afd724498ca4ed826feb5162230c199
Manoj Kumar <70743261+manojkumar8400@users.noreply.github.com>
2021-12-10 09:38:20 +0530

    minor changes

--- area.js
+++ area.js
@@ -1,4 +1,4 @@
-const sides = document.querySelectorAll("input");
+const angleSides = document.querySelectorAll("input");
 const calculate = document.querySelector("#calculate-btn");
 const output= document.querySelector("#output")
 
@@ -8,13 +8,13 @@ calculate.addEventListener("click",calculateArea);
 
 function calculateArea(){
     const area = 
-    sumOfSides(Number(sides[0].value),
-    Number(sides[1].value));
+    sumOfSides(Number(angleSides[0].value),
+    Number(angleSides[1].value));
     var areaOfTriangle = ((1/2)*area)
     
-    output.innerText="The area of the triangle is "+ String(areaOfTriangle**(0.5))
+    output.innerText="The area of the triangle is "+ String(areaOfTriangle)+"cm²"
 }
 
 function sumOfSides(){
-    return Number(sides[0].value) * Number(sides[1].value)
+    return Number(angleSides[0].value) * Number(angleSides[1].value)
 }
